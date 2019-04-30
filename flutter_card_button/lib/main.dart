10f60aeeaa20b5e82b229a33154a234d5fe39eed
Unknown <artphonegraphy@gmail.com>
2019-04-30 14:47:16 +0700

    Image

--- flutter_card_button/lib/main.dart
+++ flutter_card_button/lib/main.dart
@@ -44,68 +44,119 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+    Widget topAppBar = AppBar(
+      elevation: 0.1,
+      backgroundColor: Colors.amber,
+      title: Text("Card Button"),
+      actions: <Widget>[
+        IconButton(
+          icon: Icon(Icons.list),
+          onPressed: () {},
+        )
+      ],
+    );
+    Widget leftCard(){
+    return Container(
+      margin: EdgeInsets.only(top: 15.0, left: 10, right: 10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          Container(
+            height: 110.0,
+            width: 110.0,
+            child: InkWell(
+              child: Card(
+                child: new InkWell(
+                  child: Column(
+                    children: <Widget>[
+                      Image.asset('assets/icon1.png'),
+                      Text(
+                        "Lunch",
+                        style: TextStyle(fontWeight: FontWeight.w600),
+                      ),
+                    ],
+                  ),
+                  onTap: () {
+                    // Navigator.push(
+                    //   context,
+                    //   MaterialPageRoute(builder: (context) => Screen()),
+                    // );
+                  },
+                ),
+              ),
+            )
+          ),
+          Container(
+            height: 110.0,
+            width: 110.0,
+            child: InkWell(
+              child: Card(
+                child: new InkWell(
+                  child: Column(
+                    children: <Widget>[
+                      Image.asset('assets/Image/lunch.png'),
+                      Text(
+                        "Lunch",
+                        style: TextStyle(fontWeight: FontWeight.w600),
+                      ),
+                    ],
+                  ),
+                  onTap: () {
+                    // Navigator.push(
+                    //   context,
+                    //   MaterialPageRoute(builder: (context) => Screen()),
+                    // );
+                  },
+                ),
+              ),
+            )
+          ),
+          Container(
+            height: 110.0,
+            width: 110.0,
+            child: InkWell(
+              child: Card(
+                child: new InkWell(
+                  child: Column(
+                    children: <Widget>[
+                      Image.asset('assets/Image/lunch.png'),
+                      Text(
+                        "Lunch",
+                        style: TextStyle(fontWeight: FontWeight.w600),
+                      ),
+                    ],
+                  ),
+                  onTap: () {
+                    // Navigator.push(
+                    //   context,
+                    //   MaterialPageRoute(builder: (context) => Screen()),
+                    // );
+                  },
+                ),
+              ),
+            )
+          ),
+        ],
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      appBar: topAppBar,
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: Container(
+        color: Colors.amberAccent,
         child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
+            leftCard()
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        ),
     );
   }
 }
